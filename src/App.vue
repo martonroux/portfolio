8746b52caeea4969d754bc17bbe46e6346291ade
martonroux <marton.roux@epitech.eu>
2023-06-09 16:21:58 +0200

    [FIX] changed order in which widgets appear, to show most important widgets first

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,11 @@ import CorewarWidget from "@/components/widgets/CorewarWidget.vue";
   </header>
 
   <main>
-    <MyRPGWidget/>
+    <CorewarWidget/>
     <Need4StekWidget/>
     <XRayWidget/>
     <WeightWatchersWidget/>
-    <CorewarWidget/>
+    <MyRPGWidget/>
   </main>
 </template>
 
