d2db4af4103ee0c03e5ed52f24a15e406f69eb5e
martonroux <marton.roux@epitech.eu>
2023-09-16 20:42:20 +0200

    [ADD] new version

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,30 @@
 <script setup>
-import MyRPGWidget from "@/components/widgets/MyRPGWidget.vue";
-import Need4StekWidget from "@/components/widgets/Need4StekWidget.vue";
-import XRayWidget from "@/components/widgets/XRayWidget.vue";
-import WeightWatchersWidget from "@/components/widgets/WeightWatchersWidget.vue";
-import CorewarWidget from "@/components/widgets/CorewarWidget.vue";
+import Header from "@/components/Header.vue";
 </script>
 
 <template>
   <header>
-    <h1 style="margin-right: 1vw; margin-left: 1vw">
-      Hi, I'm Marton Roux, an Epitech Student
-    </h1>
-    <h2>
-      This is my portfolio.
-    </h2>
+    <Header />
   </header>
-
-  <main>
-    <CorewarWidget/>
-    <Need4StekWidget/>
-    <XRayWidget/>
-    <WeightWatchersWidget/>
-    <MyRPGWidget/>
-  </main>
+  <h1>
+    Titre
+  </h1>
+  <h2>
+    Sous-titre
+  </h2>
+  <h3>
+    Sous-sous-titre
+  </h3>
+  <h4>
+    Sous-sous-sous-titre
+  </h4>
+  <h5>
+    Sous-sous-sous-sous-titre
+  </h5>
+  <p>
+    Paragraphe
+  </p>
 </template>
 
 <style scoped>
-
-header {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: flex-end;
-}
-
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,119 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="header">
+    <div @click="onClick" class="menu-button">
+      <span class="line-1" />
+      <span class="line-2" />
+      <span class="line-3" />
+      <span class="line-4" />
+    </div>
+    <div class="portfolio-text">
+      <div class="portfolio-text-letter">P</div>
+      <div class="portfolio-text-letter">O</div>
+      <div class="portfolio-text-letter">R</div>
+      <div class="portfolio-text-letter">T</div>
+      <div class="portfolio-text-letter">F</div>
+      <div class="portfolio-text-letter">O</div>
+      <div class="portfolio-text-letter">L</div>
+      <div class="portfolio-text-letter">I</div>
+      <div class="portfolio-text-letter">O</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["clicked"],
+  methods: {
+    onClick() {
+      this.$emit('clicked');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.header {
+  height: var(--grid-size);
+  width: 100%;
+}
+
+.menu-button {
+  position: absolute;
+  top: calc((var(--grid-size) - (var(--grid-size) / 2)) / 2);
+  left: calc(var(--grid-size) + ((var(--grid-size) - (var(--grid-size) / 2)) / 2));
+
+  height: calc(var(--grid-size) / 2);
+  width: calc(var(--grid-size) / 2);
+
+  background-color: transparent;
+
+  cursor: pointer;
+  transition: transform 0.4s ease-in-out;
+}
+.menu-button > span {
+  height: 2px;
+  width: calc(var(--grid-size) / 2);
+  background-color: var(--white-color);
+
+  position: absolute;
+  transition: transform 0.3s ease-in-out;
+}
+.line-1 {
+  /* Ligne du haut */
+}
+.line-2 {
+  /* Ligne à droite */
+
+  transform: rotate(-90deg);
+  transform-origin: bottom right;
+}
+.line-3 {
+  /* Ligne en bas */
+
+  top: calc(var(--grid-size) / 2);
+}
+.line-4 {
+  /* Ligne à gauche */
+
+  top: calc(var(--grid-size) / 2);
+
+  transform: rotate(-90deg);
+  transform-origin: top left;
+}
+
+.portfolio-text {
+  position: absolute;
+  display: flex;
+
+  flex-direction: row;
+  gap: 2.3rem;
+
+  right: var(--grid-size);
+  transform: translateY(calc((var(--grid-size) - 100%) / 2));
+}
+.portfolio-text > div {
+  font-size: var(--h3-font-size);
+  opacity: 40%;
+}
+
+@media (hover: hover) {
+  .menu-button:hover > .line-2 {
+    transform: rotate(0) translateY(400%);
+  }
+  .menu-button:hover > .line-4 {
+    transform: rotate(0) translateY(-400%);
+  }
+  .menu-button:hover > .line-1 {
+    transform: translateY(400%);
+  }
+  .menu-button:hover > .line-3 {
+    transform: translateY(-400%);
+  }
+}
+
+</style>
